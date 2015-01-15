e1c6ed685179c4dabcee679ce86d5866b4c17358
Ryan Jansen <ryan.alan.jansen@gmail.com>
2015-01-14 19:13:59 -0800

    Add build.bat script and .nuspec file

--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
 @echo Off
+
+set SRCDIR=%~dp0src
+set PROJNAME=lib-extras
+
 set config=%1
 if "%config%" == "" (
    set config=Release
 )
  
-set version=1.0.0
+set version=0.0.0
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -14,8 +18,12 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
  
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\lib-extras.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %SRCDIR%\%PROJNAME%.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
+rmdir /s /q Build 2>nul
 mkdir Build
- 
-%nuget% pack "src\lib-extras.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
+if exist "%SRCDIR%\%PROJNAME%.nuspec" (
+  echo.
+  %nuget% pack "%SRCDIR%\%PROJNAME%.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+)
